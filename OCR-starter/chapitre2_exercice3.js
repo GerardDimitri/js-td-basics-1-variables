a09653ee2755ec3ea1c7a81fc28f72f80b212596
Gerard Dimitri <dimitri.gerard@student.hepl.be>
2025-10-01 14:07:46 +0200

    Exercices fini

--- OCR-starter/chapitre2_exercice3.js
+++ OCR-starter/chapitre2_exercice3.js
@@ -2,7 +2,7 @@
 
 let number1=5;
 let number2=9;
-let temp = null;
+let temp
 
 console.log(`Nombre de base : \nNombre 1 : ${number1}\nNombre 2 : ${number2}`);
 
--- starter/script011.js
+++ starter/script011.js
@@ -23,5 +23,5 @@ let b = 15;
 
 console.log("a > b ? " + (a > b));   // true
 console.log("a < b ? " + (a < b));   // false
-console.log("a == b ? " + (a == b)); // false
-console.log("a != b ? " + (a != b)); // true
+console.log("a == b ? " + (a === b)); // false
+console.log("a != b ? " + (a !== b)); // true
